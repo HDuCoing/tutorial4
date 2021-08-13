dec37dc8ccc2dae5761a1b035f6b16ab29381f72
Holly <71049594+HDuCoing@users.noreply.github.com>
2021-08-13 14:49:26 +1200

    Add files via upload

---
+++ src/main/java/mypackage/Calc.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+public class Calc{
+
+	public add(int a, int b) {
+		Integer add = a+b
+		return add
+	}
+	public subtract(int a, int b) {
+		Integer sub = a-b
+		return sub
+	}
+}
---
+++ src/main/java/mypackage/TestCalc.java
@@ -0,0 +1,8 @@
+package mypackage;
+
+public class TestCalc {
+	public static void(String[] args) {
+		Calc test = new Calc();
+		test;
+	}
+}
